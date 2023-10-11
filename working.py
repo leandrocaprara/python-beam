6e80b03bbe00d83e4cfb6eab24f5998f60783a5f
Leandro Caprara Costa <44200777+leandrocaprara@users.noreply.github.com>
2023-10-11 18:42:18 -0300

    The code on this file works with Python and Apache Beam

---
+++ working.py
@@ -0,0 +1,69 @@
+import os
+import time
+import threading
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+# Diretório raiz a ser monitorado
+root_directory = "C:\\rj227\\data_source"
+
+# Pipeline Beam para processar os caminhos absolutos
+
+
+class PathExtractor(beam.DoFn):
+    def process(self, element):
+        yield element
+
+
+# def monitor_directory(event):
+#     if event.is_directory:
+#         print(f'Diretório: {event.src_path}')
+#     else:
+#         print(f'Arquivo: {event.src_path}')
+
+
+class CustomFileSystemEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            print(f'Novo diretório criado: {event.src_path}')
+        else:
+            print(f'Novo arquivo criado: {event.src_path}')
+
+
+def run_beam_pipeline():
+    options = PipelineOptions([])
+    with beam.Pipeline(options=options) as p:
+        _ = (
+            p
+            | "Generate paths" >> beam.Create([root_directory])
+            | "Find paths" >> beam.FlatMap(lambda path: [os.path.join(dp, f) for dp, dn, filenames in os.walk(path) for f in filenames])
+            | "Extract paths" >> beam.ParDo(PathExtractor())
+        )
+
+
+def start_beam_pipeline():
+    while True:
+        # Aguarde um segundo para evitar consumo excessivo de CPU
+        time.sleep(1)
+        run_beam_pipeline()
+
+
+if __name__ == "__main__":
+    event_handler = CustomFileSystemEventHandler()
+
+    observer = Observer()
+    observer.schedule(event_handler, path=root_directory, recursive=True)
+    observer.start()
+
+    beam_thread = threading.Thread(target=start_beam_pipeline)
+    beam_thread.daemon = True
+    beam_thread.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
